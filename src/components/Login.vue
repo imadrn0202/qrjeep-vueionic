b5ca6e1ee33d8649b1de3960ddd3f8403d7c1252
imadrn0202 <55490404+imadrn0202@users.noreply.github.com>
2019-10-12 18:51:39 +0800

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+<ion-grid>
+    <form>
+            <ion-col>
+                <ion-item>
+                    <ion-label>Username: </ion-label>
+                    <ion-input type="text" name="username"></ion-input>
+                </ion-item>
+                <ion-item>
+                    <ion-label>Password: </ion-label>
+                    <ion-input type="text" name="password"></ion-input>
+                </ion-item>
+            </ion-col>
+            <ion-col>
+                <ion-item>
+                    <ion-button type="submit" color="primary" expand="block">
+                        Login
+                    </ion-button>
+                </ion-item>
+            </ion-col>
+    </form>
+</ion-grid>
+</template>
+
+<script>
+export default {
+    name: 'Login'
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>QR Jeep</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <Login/>
+    </ion-content>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import Login from '../components/Login'
+export default {
+  name: 'home',
+  components: {
+    Login
+  }
+}
+</script>
